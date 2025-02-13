4d0b67c25915ad1bdfa54eded663a9966afb515e
1194713895 <1194713895@qq.com>
2025-02-13 23:00:42 +0800

    添加 15_2.cpp 文件，包含基本的输入输出和字符串处理库

---
+++ 15_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>  // 用于文件输入输出
+#include <string>
+#include <sstream>  // 用于字符串流
+using namespace std;
+
+// long long current_time;
+long long last_time;
+
+int n=0;
+int max_n=0;
+
+int main() {
+   
+    return 0;
+}
