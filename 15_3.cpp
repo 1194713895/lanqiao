e43ee5ba4f0946093876dac5203f1053bbf10b8e
1194713895 <1194713895@qq.com>
2025-02-20 20:56:41 +0800

    在 15_3.cpp 中添加输入处理逻辑，并更新 .vscode/settings.json 文件以改善文件关联

--- 15_3.cpp
+++ 15_3.cpp
@@ -3,15 +3,46 @@
 #include <string>
 #include <sstream>  // 用于字符串流
 #include <time.h>
+#include <cmath>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
+
+long long n = 0;
+
+long long a[200000];
+
+int count = 0;
+
+int num = 0;
+
+char str[20]="zlm";
+// string str = "zlm";
 
 int main() {
 
     // 获取开始时间
     clock_t start = clock();
 
-   
-   
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sprintf(str, "%lld", a[i]);
+        num = strlen(str);
+        cout << num << endl;
+        memset(str,0,sizeof(str));
+
+    }
+
+    cout << str << endl;
+    
+    // // memset(str,0,sizeof(str));
+    // // str.resize(0);
+
+    // cout << str << endl;
+
     // 获取结束时间
     clock_t end = clock();
 
