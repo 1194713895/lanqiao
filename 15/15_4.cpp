b939efa0e71b492ec87a36774051a26b6e75c913
1194713895 <1194713895@qq.com>
2025-02-22 19:32:18 +0800

    add

--- 15/15_4.cpp
+++ 15/15_4.cpp
@@ -11,7 +11,22 @@ using namespace std;
 
 int main()
 {
-    float a=log(100);
-    cout << a;
+    int n = 0;
+    int m = 0;
+
+    cin >> n >> m;
+    
+    char a[n][m];
+
+    for(int i =0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    
+
     return 0;
 }
