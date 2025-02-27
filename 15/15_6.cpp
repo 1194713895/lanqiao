c4aba4cf65cb36f79f45e98cf8df122c7216b96c
1194713895 <1194713895@qq.com>
2025-02-27 16:39:34 +0800

    add

--- 15/15_6.cpp
+++ 15/15_6.cpp
@@ -4,9 +4,40 @@
 
 using namespace std;
 
+struct chd{
+    int r;
+    int c;
+};
+
 int main()
 {
-    
+    int n,m,T;
+    cin>>n>>m>>T;
+
+    vector<vector<chd>> a(n,vector<chd>(m));
+    vector<chd> b(T);
+    vector<chd> c(T);
+
+    for(int i=0;i<n;i++){
+        int tmp;
+        cin>>tmp;
+        for(int j=0;j<m;j++){
+            a[i][j].r=tmp;
+        }
+    }
+    for(int i=0;i<m;i++){
+        int tmp;
+        cin>>tmp;
+        for(int j=0;j<n;j++){
+            a[j][i].c=tmp;
+        }
+    }
+    for(int i=0;i<T;i++){
+        cin>>b[i].r;
+        cin>>b[i].c;
+        cin>>c[i].r;
+        cin>>c[i].c;
+    }
 
     return 0;
 }
