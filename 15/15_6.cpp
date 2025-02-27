9baf97f9b3577aea57367af1adc3eb68a7d5684f
1194713895 <1194713895@qq.com>
2025-02-27 16:52:24 +0800

    add

--- 15/15_6.cpp
+++ 15/15_6.cpp
@@ -38,6 +38,23 @@ int main()
         cin>>c[i].r;
         cin>>c[i].c;
     }
+    cout<<endl;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<a[i][j].r<<" "<<a[i][j].c<<"   ";
+        }
+       
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=0;i<T;i++){
+        cout<<i<<": "<<b[i].r<<" "<<b[i].c<<"   ";
+    }
+    cout<<endl;
+    for(int i=0;i<T;i++){
+        cout<<i<<": "<<c[i].r<<" "<<c[i].c<<"   ";
+    }
 
     return 0;
 }
